fee812ca1a85723ab8bfeda255ae8d24976f74e6
Patipuu <147331118+Patipuu@users.noreply.github.com>
2023-11-21 08:10:45 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,8 +4,6 @@ const registerLink=document.querySelector('.register-link');
 const ForgotPassLink=document.querySelector('.ForgotPassword-link');
 const btnPopup=document.querySelector('.btnLogin-popup');
 const iconclose=document.querySelector('.icon-close');
-
-
 //Bấm vào để chuyển qua trang đăng ký
 registerLink.addEventListener('click',()=>{
     wrapper.classList.add('active');
@@ -56,8 +54,7 @@ function arePasswordsMatching(password, checkpassword)
 
 
 
-//Biến để kiểm tra đã có tài khoản hay chưa
-var isUser = false;
+
 
 
 
@@ -98,9 +95,11 @@ function signup() {
       alert("Xác nhận mật khẩu chưa chính xác!");
       return;
   }
-
-  // Add a new field for user role (default to 'user')
-  var role = 'user';
+ 
+// <------------------------------------------------->
+  // Tạo tài khoản bằng đăng kí
+  if(email === "Admin123@gmail.com")
+  var role = 'Admin'; else var role = 'User';
 
   // Tạo 1 đối tượng tài khoản mới
   var account = {
@@ -110,10 +109,10 @@ function signup() {
       registrationDate: formattedDate,
       role: role  // Include the role field
   };
-
+ 
   // Kiểm tra tài khoản đã nằm trong localStorage chưa
   var accounts = JSON.parse(localStorage.getItem('users')) || [];
-
+  
   // Thêm tài khoản mới vào mảng
   accounts.push(account);
 
@@ -121,10 +120,6 @@ function signup() {
   localStorage.setItem('users', JSON.stringify(accounts));
 
   if (password === checkpassword) {
-
-      // Đặt trạng thái đăng nhập thành true sau khi đăng ký thành công
-      isUser = true;
-      
       // Hiển thị thông báo thành công
       alert('Tài khoản đã tạo thành công');
   } else {
@@ -142,14 +137,16 @@ function login(event) {
   var users = JSON.parse(localStorage.getItem('users')) || [];
 
   // Tìm email và mật khẩu trong localstorage
-  var loggedInUser = users.find(user => user.email === email1 && user.password === password1);
-
-  if (loggedInUser) {
-
-      // Đặt trạng thái đăng nhập thành true sau khi đăng nhập thành công
-      isUser = true;
-
-      alert(`Đăng nhập thành công! Chào mừng ${loggedInUser.role === 'admin' ? 'Admin' : 'Người dùng'}`);
+  var loggedInUser = users.find(user => user.email === email1 && user.password === password1 && user.role === "User" );
+  var loggedInAdmin = users.find(user => user.email === email1 && user.password === password1 && user.role === "Admin" );
+  
+  if(loggedInAdmin)
+  {
+    alert("Đăng nhập thành công!, Chào Admin");
+      window.location.href="Layout admin.html" ;
+  }  
+  else if (loggedInUser) {
+      alert("Đăng nhập thành công!");
       window.location.href="Trangchu_DoAn.html" ;
   } else {
     
@@ -159,14 +156,18 @@ function login(event) {
 
 
 
-//Kiểm tra trạng thái đăng nhập trước khi mua
-function buyProduct() {
-  if (isUser) {
-      // Thực hiện quy trình mua sản phẩm
-      alert('Bạn đã mua sản phẩm thành công!');
-  } else {
-      // Hiển thị thông báo yêu cầu đăng nhập
-      alert('Vui lòng đăng nhập để mua sản phẩm');
-      return;
-  }
+/*
+var regex = /^(?=.*d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+if(regex.test(password)) {
+  console.log("Password is valid");
+} else {
+  console.log("Password is invalid");
+} */
+
+
+/* 
+function checkPassword(password) {
+ var regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+ return regex.test(password);
 }
+*/
